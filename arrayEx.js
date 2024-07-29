c84d43aa8b84e8d25d064f8654064750c5c83be3
João <claudiopopbr@gmail.com>
2024-07-29 19:57:42 -0300

    Atividade 29/07/24

---
+++ arrayEx.js
@@ -0,0 +1,10 @@
+var matrix = Array()
+
+matrix = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 9, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]]
+
+for (i = 0; i < matrix.length; i++) {
+
+    for (j = 0; j < matrix[i].length; j++)
+
+        console.log(matrix[i][j])
+}
---
+++ arrayTestes.js
@@ -0,0 +1,19 @@
+// var listaDeCarros = Array()
+// listaDeCarros = ["Honda", "Chevrolet", "Gol"]
+// console.log(listaDeCarros[1]) Exemplo de Array 1
+
+
+// var listaDeFruta = Array()
+// listaDeFruta [1] = "banana"
+// listaDeFruta [2] = "morango"
+// listaDeFruta [5] = "manga"
+// listaDeFruta [9] = "acerola"
+// listaDeFruta [6] = "melão"
+// console.log(listaDeFruta[1])
+
+var listaDeNome = Array()
+
+listaDeNome ["Nomes"] = ["Danielly", "João", "Rafael", "Yasmin"]
+listaDeNome ["Sobrenomes"] = ["Chagas", "Cavalcanti", "Nunes", "Gabriele"]
+
+console.log(listaDeNome ["Nomes"] [1])
