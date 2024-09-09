81b6a6bf3383028d3855dcb5ce592cd0a560f785
João <joaocavalcanti2003@gmail.com>
2024-09-09 19:57:14 -0300

    Começando em TS

---
+++ Typescript/socorro.ts
@@ -0,0 +1,26 @@
+class Bolacha {
+
+    protected marca: string
+
+    constructor(marca:string) {
+        this.marca = marca
+    }
+}
+
+class Tipo extends Bolacha {
+    private sabor: string 
+
+    constructor(marca:string, sabor:string) {
+        super(marca)
+        this.sabor = sabor
+    }
+
+    public exibirInfo() : void {
+
+        console.log("A bolacha ", this.marca, "  do sabor ", this.sabor, ".")
+    }
+
+}
+
+let Comida = new Bolacha ("Treloso")
+console.log(Comida)
